294f7d406078c7b77636156da3ceca9cc6323f2a
Marta Tsiruh <martatsiruch@gmail.com>
2022-05-09 23:25:38 +0200

    add notification module

---
+++ src/components/Feedback/Notification/Notification.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import s from './Notification.module.css';
+
+const Notification = ({ message }) => {
+  return <h3 className={s.title}>{message}</h3>;
+};
+
+
+Notification.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+export default Notification;
