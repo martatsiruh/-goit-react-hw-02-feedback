93fe6f3ab6bed0e6e4ba8473c38ab85e8bd7e71c
Marta Tsiruh <martatsiruch@gmail.com>
2022-05-09 23:24:29 +0200

    add section module

---
+++ src/components/Feedback/Section/Section.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import s from './Section.module.css';
+
+const Section = ({ title, children }) => {
+  return (
+    <>
+      <h2 className={s.title}>{title}</h2>
+      {children}
+    </>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Section;
